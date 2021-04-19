4af1a6fc078af811d7b3f319fa70e47be7dd017b
Meharpreet Singh Nanda <msnanda515@gmail.com>
2021-04-19 16:43:16 -0600

    Completed Coin Pile

--- DailyChallenge/Cpp/coinPiles.cpp
+++ DailyChallenge/Cpp/coinPiles.cpp
@@ -7,17 +7,20 @@ int main(){
     long a,b;
     for (int i = 0; i < n; i++){
         cin >> a >> b;
+        long temp;
+        temp = min(a,b);
+        b = max(a,b);
+        a = temp;
         if (a == 2 * b)
-            cout << "YES" << endl;
-        // else if (a == 0 || b == 0)
-        //     cout << "YES" << endl; 
+            cout << "YES" << endl; 
         else if (b == 2 * a)
             cout << "YES" << endl;
-        else if (a%3 == 0 && b%3==0 && a!=0 && b!=0)
-            cout << "YES" << endl;
-        else if (((a%3 == 1 && b%3 == 2) || (a%3==2 && b%3==1)) && abs(a-b)<min(a,b))
-            cout << "YES" << endl;
-        else    
-            cout << "NO" << endl;
+        else{
+            long x = b - a;
+            if ((a - x)%3==0 && (b-2*x)%3==0 && x <= a)
+                cout << "YES" << endl;
+            else 
+                cout << "NO" << endl;
+        }
     }
 }
