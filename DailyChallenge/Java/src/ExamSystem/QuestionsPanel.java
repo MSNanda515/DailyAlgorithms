64ad5f1847bf1c8fd1aae547ad49b3a2aaecf0ac
Meharpreet Singh Nanda <msnanda515@gmail.com>
2021-05-24 11:43:29 -0600

    Resolved Project Issues and added Interface to go from one panel to another

---
+++ DailyChallenge/Java/src/ExamSystem/QuestionsPanel.java
@@ -0,0 +1,50 @@
+package ExamSystem;
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class QuestionsPanel extends JPanel{
+    ArrayList<Question> ques;
+    ArrayList<String[]> ans;
+    ArrayList<String> correctAns;
+
+    QuestionsPanel() {
+
+        JLabel l1 = new JLabel("HELLO THERE");
+        l1.setBounds(10, 10, 100, 30);
+        this.add(l1);
+
+        this.setSize(400, 400);
+        this.setLayout(null);
+        this.setVisible(true);
+    }
+
+    void defineQuesAns() {
+        // Make the ques and answer objects and use them
+        ques = new ArrayList<>();
+        ans = new ArrayList<>();
+        correctAns = new ArrayList<>();
+        for (int i = 0; i < 5; i++){
+            String tempQues = "What is your name?";
+            Question q = new Question(Question.SINGLE_CORRECT, tempQues);
+            String[] tempAns = {"M1", "M2", "M3", "M4"};
+            String corAns = "M2";
+            ques.add(q);
+            ans.add(tempAns);
+            correctAns.add(corAns);
+        }
+    }
+
+    class Question {
+        static final int SINGLE_CORRECT = 1;
+        static final int MULTI_CORRECT = 2;
+        public int type;
+        public String ques;
+        Question(int type, String ques) {
+            this.type = type;
+            this.ques = ques;
+        }
+    }
+}
