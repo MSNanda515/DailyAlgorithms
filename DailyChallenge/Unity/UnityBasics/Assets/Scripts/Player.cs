1865b2e7d1c527da4ebadeb031a3c5a8563c4086
Meharpreet Singh Nanda <msnanda515@gmail.com>
2021-05-26 15:22:58 -0600

    Made my first Unity Game

--- DailyChallenge/Unity/UnityBasics/Assets/Scripts/Player.cs
+++ DailyChallenge/Unity/UnityBasics/Assets/Scripts/Player.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private bool jumpKeyWasPressed;
+    private float horizontalInput;
+    private int superJumpsRemaining = 0;
+    private Rigidbody rigidBodyComponent;
+    [SerializeField] private Transform groundCheckTransform = null;
+    [SerializeField] private LayerMask playerMask;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBodyComponent = GetComponent<Rigidbody>();   
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Jump when space key is pressed
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            jumpKeyWasPressed = true;
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
         
     }
+
+    // FixedUpdate is called once every Physics Update
+    private void FixedUpdate()
+    {
+        rigidBodyComponent.velocity = new Vector3(horizontalInput, rigidBodyComponent.velocity.y, 0);
+        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 0)
+        {
+            return;
+        }       
+        if (jumpKeyWasPressed)
+        {
+            float jumpPower = 5;
+            if (superJumpsRemaining > 0)
+            {
+                jumpPower *= 2;
+                superJumpsRemaining--;
+            }
+            GetComponent<Rigidbody>().AddForce(Vector3.up * jumpPower, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 9)
+        {
+            Destroy(other.gameObject);
+            superJumpsRemaining++;
+        }
+    }
+
+
 }
