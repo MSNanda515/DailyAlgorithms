134fed4fe50b5f87be7f24c5ecb74bbfe36cf13b
Meharpreet Singh Nanda <msnanda515@gmail.com>
2021-06-11 12:18:11 -0600

    working on ajax part

--- DailyChallenge/Python/SimpleChatApp/website/static/index.js
+++ DailyChallenge/Python/SimpleChatApp/website/static/index.js
@@ -1,12 +1,30 @@
+// $(function() {
+//   $('#sendBtn').bind('click', function() {
+//     $.getJson('/run',
+//       {
+//         value: va
+//       },
+//       function(data) {
+//       value = document.getElementById("msg").value
+//     });
+//     return false;
+//   });
+// });
+
 $(function() {
   $('#sendBtn').bind('click', function() {
-    $.getJson('/run',
-      function(data) {
-      console.log("test")
-    });
-    return false;
+      var msg = document.getElementById("msg")
+      var value = msg.value
+      msg.value = ""
+
+      $.getJSON('/send_message',
+          {val:value},
+          function(data) {
+
+          });
   });
 });
+
 
 function validate(name) {
   if (name.length >= 2) {
