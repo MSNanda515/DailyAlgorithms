28c51618b4a0c1335a1430084092aa3cf2be0d63
Meharpreet Singh Nanda <msnanda515@gmail.com>
2021-08-18 09:00:00 -0600

    Memory intensive approach on dislikeofthrees

---
+++ Cpp/CodeForces/Dislikeofthrees.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  int k = 0;
+  int el[1000];
+  for (int i = 1; k < 1000; i++) {
+    if (!(i % 3 == 0 || i % 10 == 3)) {
+      // Valid no for the sequence
+      el[k++] = i;
+    }
+  }
+  int t;
+  cin >> t;
+  for (int i = 0; i < t; i++) {
+    int query;
+    cin >> query;
+    cout << el[query - 1] << '\n';
+  }
+}
