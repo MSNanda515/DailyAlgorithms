1a554e0124f8832522891663a9583a8c2934ec56
Meharpreet Singh Nanda <msnanda515@gmail.com>
2022-04-15 15:08:03 -0600

    Learn about Kadane's algo

---
+++ geeksfg/kadaneAlgo.py
@@ -0,0 +1,12 @@
+def max_subarray(numbers):
+    """Find the largest sum of any contiguous subarray."""
+    best_sum = 0
+    current_sum = 0
+    for x in numbers:
+        current_sum = max(0, current_sum + x)
+        best_sum = max(best_sum, current_sum)
+    return best_sum
+
+if __name__ == "__main__":
+    numbers = list(map(int, input().split()))
+    print(max_subarray(numbers))
